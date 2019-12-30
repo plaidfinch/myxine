8f7fdaf50be66e7c9524b52c9658337a8451b2a1
Kenny Foner <kwf@galois.com>
2019-12-30 14:21:28 -0500

    Documentation for the params module

--- src/params.rs
+++ src/params.rs
@@ -6,6 +6,7 @@ use std::borrow::Cow;
 pub(crate) struct GetParams;
 
 impl GetParams {
+    /// Parse a query string from a GET request.
     pub fn parse(query: &str) -> Option<GetParams> {
         let params = query_params(query)?;
         if constrained_to_keys(&params, &[]) {
@@ -16,7 +17,7 @@ impl GetParams {
     }
 }
 
-/// Parsed parameters from a query string for a POST request of a .
+/// Parsed parameters from a query string for a POST request.
 pub(crate) enum PostParams {
     Dynamic{title: String},
     Static,
@@ -24,6 +25,7 @@ pub(crate) enum PostParams {
 }
 
 impl PostParams {
+    /// Parse a query string from a POST request.
     pub fn parse(query: &str) -> Option<PostParams> {
         let params = query_params(query)?;
         if let Some([]) = params.get("subscribe").map(Vec::as_slice) {
@@ -49,6 +51,9 @@ impl PostParams {
     }
 }
 
+/// Parse a given parameter as a boolean, where its presence without a mapping
+/// is interpreted as true. If it is mapped to multiple values, or mapped to
+/// something other than "true" or "false", return `None`.
 fn param_as_bool<'a, 'b: 'a>(param: &'b str, params: &'a HashMap<&'a str, Vec<Cow<'a, str>>>) -> Option<bool> {
     match params.get(param).map(Vec::as_slice) {
         Some([boolean]) => match boolean.as_ref() {
@@ -62,6 +67,9 @@ fn param_as_bool<'a, 'b: 'a>(param: &'b str, params: &'a HashMap<&'a str, Vec<Co
     }
 }
 
+/// Parse a given parameter as a string, where its presence without a mapping
+/// (or its absence entirely) is interpreted as the empty string. If it is
+/// mapped to multiple values, retrun `None`.
 fn param_as_str<'a, 'b: 'a>(param: &'b str, params: &'a HashMap<&'a str, Vec<Cow<'a, str>>>) -> Option<&'a str> {
     match params.get(param).map(Vec::as_slice) {
         Some([string]) => Some(string.as_ref()),
@@ -70,7 +78,11 @@ fn param_as_str<'a, 'b: 'a>(param: &'b str, params: &'a HashMap<&'a str, Vec<Cow
     }
 }
 
-
+/// Parse a query string into a mapping from key to list of values. The syntax
+/// expected for an individual key-value mapping is one of `k`, `k=`, `k=v`,
+/// `k=v1,v2`, etc., and mappings are concatenated by `&`, as in:
+/// `k1=v1,v2&k2=v3,v4`. Values are URL-percent-decoded prior to being returned,
+/// whereas keys are required to be URL-safe strings.
 fn query_params<'a>(query: &'a str) -> Option<HashMap<&'a str, Vec<Cow<'a, str>>>> {
     let mut map: HashMap<&'a str, Vec<Cow<'a, str>>> = HashMap::new();
     if query == "" { return Some(map); }
@@ -95,6 +107,8 @@ fn query_params<'a>(query: &'a str) -> Option<HashMap<&'a str, Vec<Cow<'a, str>>
     Some(map)
 }
 
+/// If the keys of the hashmap are exclusively within the set enumerated by the
+/// slice, return `true`, otherwise return `false`.
 fn constrained_to_keys<'a, K: PartialEq + 'a, V>(
     map: &HashMap<K, V>,
     valid: &[K]
