b51eb16b87e078dcf7b6408c584f5f75df2d9262
Kenny Foner <kwf@galois.com>
2020-01-01 17:46:10 -0500

    Free hash table memory in pages table during heartbeat cycle

--- src/heartbeat.rs
+++ src/heartbeat.rs
@@ -72,6 +72,11 @@ pub async fn heartbeat_loop() {
             for path in pruned.lock().await.iter() {
                 paths.remove(path);
             }
+
+            // Free memory for all the removed pages and paths
+            paths.shrink_to_fit();
+            PAGES.lock().await.shrink_to_fit();
+            // TODO: do same pruning on the subscriber lists inside pages
         }
     };
 
