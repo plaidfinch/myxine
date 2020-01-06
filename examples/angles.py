84dd5f911ac8236e8e3cefdd9f4dc22f35a757b4
Kenny Foner <kwf@galois.com>
2020-01-06 14:58:38 -0500

    Add python library for interacting with myxine, first python example

---
+++ examples/angles.py
@@ -0,0 +1,110 @@
+#! /usr/bin/env python3
+from math import *
+import myxine
+
+class State:
+    # The dimensions of the browser window
+    (w, h) = (1, 1)
+    # The location of the cursor relative to the browser window
+    (x, y) = (0, 0)
+
+    def update(self, event):
+        w = event['window.innerWidth']
+        h = event['window.innerHeight']
+        x = event['.clientX']
+        y = event['.clientY']
+        if w is not None: self.w = w
+        if h is not None: self.h = h
+        if x is not None: self.x = x
+        if y is not None: self.y = y
+
+    def draw(self):
+        angle = round(degrees(atan2(self.y - self.h/2,
+                                    self.x - self.w/2)) + 90)
+        if angle < 0: angle = angle + 360
+        ratio_from_edge = \
+            1 - (abs(self.y - self.h/2) +
+                 abs(self.x - self.w/2)) / (self.h/2 + self.w/2)
+        saturation = 100 * ratio_from_edge
+        lightness = 100 - 50 * ratio_from_edge
+
+        container_style = """
+        overflow: hidden;
+        margin: 0px;
+        padding: 0px;
+        height: 100vh;
+        width: 100vw;
+        background: hsl({angle}, {saturation}%, {lightness}%);
+        text-align: center;
+        position: relative;
+        """.format(angle = angle,
+                   saturation = saturation,
+                   lightness = lightness)
+        span_style = """
+        position: absolute;
+        top: 50%;
+        transform: translate(-50%, -50%) rotate({angle}deg);
+        font-family: Helvetica Neue;
+        font-weight: 200;
+        font-size: 250pt;
+        color: white;
+        background: rgba(0, 0, 0, 0.4);
+        border-radius: 300pt;
+        border: none;
+        padding: 100pt;
+        width: 550pt;
+        text-shadow: 0 0 25pt black;
+        """.format(angle = angle)
+        html = "                                           \
+        <div id=\"container\" style=\"{container_style}\"> \
+            <span style=\"{span_style}\">                  \
+                {angle}°                                   \
+            </span>                                        \
+        </div>".format(container_style = container_style,
+                       span_style = span_style,
+                       angle = angle)
+        return html
+
+# A description of the events we wish to monitor
+subscription = {
+    # Look for events on this element (id="container")
+    '#container': {
+        # Look for this event:
+        'mousemove': [
+            # Report these properties back when the event fires:
+            '.clientX', # Property of the event
+            '.clientY',
+            'window.innerHeight', # Sub-property of top-level object
+            'window.innerWidth'
+        ],
+    },
+    'window': {
+        'resize': [
+            'window.innerHeight',
+            'window.innerWidth'
+        ]
+    }
+}
+
+def main():
+    try:
+        # The path we want to serve the page at
+        path = '/'
+        print('Running at:', myxine.page_url(path))
+
+        # Make a new state object
+        state = State()
+
+        # Draw the page for the first time
+        myxine.update(path, state.draw())
+
+        # Iterate over all page events, updating the page each time
+        for event in myxine.subscribe(path, subscription):
+            state.update(event)
+            myxine.update(path, state.draw())
+
+    # You can kill the program with a keyboard interrupt
+    except KeyboardInterrupt: pass
+    except Exception as e: print(e)
+
+if __name__ == '__main__': main()
---
+++ examples/myxine.py
@@ -0,0 +1,125 @@
+from typing import Optional, Iterator, Dict, List, Any
+from dataclasses import *
+import requests
+import json
+
+@dataclass
+class Event:
+    """An event, as parsed from a text/event-stream"""
+    event: Optional[str] = None
+    id:    Optional[str] = None
+    data:  Optional[str] = None
+    retry: Optional[int] = None
+
+def parse_event_stream(lines : Iterator[str]) -> Iterator[Event]:
+    """Parse an iterable of lines from a stream conforming to the
+    text/event-stream format, as defined in the HTML living standard section
+    on server-sent events:
+    https://html.spec.whatwg.org/multipage/server-sent-events.html#parsing-an-event-stream"""
+    within_event = False
+    event = Event()
+    for line in lines:
+        if line == '' and within_event:
+            # Dispatch the event when we hit an empty line
+            yield event
+            # Reset and loop
+            within_event = False
+            event = Event()
+        elif line.startswith(':'):
+            # This is a comment line
+            pass
+        else:
+            # We are now processing an event
+            within_event = True
+
+            # Pre-process the line to extract the field name and value
+            try:
+                field, value = line.split(':', maxsplit=1)
+            except ValueError:
+                field = line
+                value = ''
+            if len(value) > 0 and value[0] == ' ':
+                value = value[1:]
+
+            # Dispatch on field name to set fields of the Event
+            if field == 'event':
+                event.event = value
+            elif field == 'id' and '\0' not in value:
+                event.id = value
+            elif field == 'data':
+                if event.data is None:
+                    event.data = value
+                else:
+                    event.data += value + '\n'
+            elif field == 'retry':
+                try:
+                    event.retry = int(value)
+                except ValueError: pass
+
+# The default port on which myxine operates; can be overridden in the below
+# functions if the server is running on another port.
+MYXINE_DEFAULT_PORT = 1123
+
+class PageEvent:
+    """A PageEvent is a special case of an event from a text/event-stream,
+    where the data is a dictionary from strings to values, representing the
+    fields requested by the subscription.
+    """
+    event : str
+    id : str
+    __mapping : Dict[str, Any]
+
+    def __init__(self, wrapped : Event) -> None:
+        if wrapped.event is not None:
+            self.event = wrapped.event
+        else:
+            self.event = ''
+        if wrapped.id is not None:
+            self.id = wrapped.id
+        else:
+            self.id = ''
+        if wrapped.data is not None:
+            self.mapping = json.loads(wrapped.data)
+        else:
+            self.mapping = {}
+
+    def __getitem__(self, key : str) -> Optional[Any]:
+        return self.mapping[key]
+
+def page_url(path : str, port : int = MYXINE_DEFAULT_PORT) -> str:
+    """Normalize a port & path to give the localhost url for that location."""
+    if len(path) > 0 and path[0] == '/': path = path[1:]
+    return 'http://localhost:' + str(port) + '/' + path
+
+def subscribe(path : str,
+              subscription : Dict[str, Dict[str, List[str]]],
+              port : int = MYXINE_DEFAULT_PORT) -> Iterator[PageEvent]:
+    """Subscribe to a stream of page events from a myxine server, returning an
+    iterator over the events returned by the stream as they become available.
+    """
+    url = page_url(path, port) + '?subscribe'
+    r = requests.post(url, stream=True, json=subscription)
+    if r.encoding is None: r.encoding = 'utf-8'
+    for event in parse_event_stream(r.iter_lines(decode_unicode=True)):
+        yield PageEvent(event)
+
+def update(path : str,
+           body : str,
+           title : str = '',
+           port : int = MYXINE_DEFAULT_PORT) -> None:
+    """Set the contents of the page at the given path to a provided body and
+    title. If body or title is not provided, clears those elements of the page.
+    """
+    url = page_url(path, port)
+    r = requests.post(url, data=body.encode(), params={'title': title})
+
+def static(path : str,
+           body : bytes,
+           content_type : str,
+           port : int = MYXINE_DEFAULT_PORT) -> None:
+    """Set the contents of the page at the given path to the static content
+    provided, as a bytestring. You must specify a content type, or else the
+    browser won't necessarily know how to display this content.
+    """
+    url = page_url(path, port) + '?static'
+    requests.post(url, data=body, headers={'Content-Type': content_type})
