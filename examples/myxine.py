12fd8c4b85c9e447e19a50021ba60088f84b7256
Kenny Foner <kwf@galois.com>
2020-01-07 14:06:03 -0500

    Simplify event parsing even more

--- examples/myxine.py
+++ examples/myxine.py
@@ -7,45 +7,33 @@ import json
 @dataclass
 class Event:
     """An event, as parsed from a text/event-stream"""
-    event: Optional[str] = None
-    id:    Optional[str] = None
-    data:  Optional[str] = None
-    retry: Optional[int] = None
+    event: str = ''
+    id:    str = ''
+    data:  str = ''
 
 def parse_event_stream(lines : Iterator[str]) -> Iterator[Event]:
-    """Parse an iterable of lines from a stream conforming to the
-    text/event-stream format, as defined in the HTML living standard section
-    on server-sent events:
-    https://html.spec.whatwg.org/multipage/server-sent-events.html#parsing-an-event-stream"""
+    """Parse an iterable of lines from the text/event-stream format, as
+    defined by the W3C Recommendation on Server-Sent Events:
+    https://www.w3.org/TR/eventsource/#event-stream-interpretation
+    """
     event = Event()
     for line in lines:
         if line == '':
-            # Dispatch the event when we hit an empty line
-            yield event
-            # Reset and loop
-            event = Event()
+            yield event # Dispatch the event
+            event = Event() # Reset and continue
         else:
-            # Pre-process the line to extract the field name and value and trim
-            # the left-most space (if any) from the value
+            # Extract the field name and value
             try: field, value = line.split(':', maxsplit=1)
             except ValueError:
                 field = line
                 value = ''
             if len(value) > 0 and value[0] == ' ':
-                value = value[1:]
+                value = value[1:] # Trim left-most space in the value, if any
 
-            # Dispatch on field name to set fields of the Event
-            if field == 'event':
-                event.event = value
-            elif field == 'id' and '\0' not in value:
-                event.id = value
-            elif field == 'data':
-                if event.data is None:
-                    event.data = value
-                else: event.data += value + '\n'
-            elif field == 'retry':
-                try: event.retry = int(value)
-                except ValueError: pass
+            # Set the appropriate field of the event, if any
+            if   field == 'event': event.event = value
+            elif field == 'id':    event.id    = value
+            elif field == 'data':  event.data += value + '\n'
 
 # The default port on which myxine operates; can be overridden in the below
 # functions if the server is running on another port.
@@ -56,31 +44,33 @@ class PageEvent:
     where the data is a dictionary from strings to values, representing the
     fields requested by the subscription.
     """
-    __event : str
-    __id : str
-    __mapping : Dict[str, Any]
+    __event: str
+    __id:    str
+    __data:  str
+    __mapping: Optional[Dict[str, Any]] = None
 
     def __init__(self, wrapped : Event) -> None:
-        if wrapped.event is not None:
-            self.__event = wrapped.event
-        else: self.__event = ''
-        if wrapped.id is not None:
-            self.__id = wrapped.id
-        else: self.__id = ''
-        if wrapped.data is not None:
-            self.mapping = json.loads(wrapped.data)
-        else: self.mapping = {}
-
-    def __getitem__(self, key : str) -> Optional[Any]:
-        return self.mapping.get(key)
+        self.__event = wrapped.event
+        self.__id    = wrapped.id
+        self.__data  = wrapped.data
 
     def event(self) -> str:
-        """Get the event type for this page event."""
+        """Get the event type of this event."""
         return self.__event
 
     def id(self) -> str:
-        """Get the target id for this page event."""
+        """Get the event id of this event."""
         return self.__id
+
+    def data(self) -> str:
+        """Get the raw data of this event as a string."""
+        return self.__data
+
+    def __getitem__(self, key : str) -> Optional[Any]:
+        if self.__mapping is None:
+            try: self.__mapping = json.loads(self.__data)
+            except: self.__mapping = {}
+        return self.__mapping.get(key)
 
 def page_url(path : str, port : int = MYXINE_DEFAULT_PORT) -> str:
     """Normalize a port & path to give the localhost url for that location."""
@@ -93,11 +83,11 @@ def subscribe(path : str,
     """Subscribe to a stream of page events from a myxine server, returning an
     iterator over the events returned by the stream as they become available.
     """
+    url = page_url(path, port) + '?subscribe'
     try:
-        url = page_url(path, port) + '?subscribe'
-        r = requests.post(url, stream=True, json=subscription)
-        if r.encoding is None: r.encoding = 'utf-8'
-        for event in parse_event_stream(r.iter_lines(decode_unicode=True)):
+        response = requests.post(url, stream=True, json=subscription)
+        if response.encoding is None: response.encoding = 'utf-8'
+        for event in parse_event_stream(response.iter_lines(decode_unicode=True)):
             yield PageEvent(event)
     except RequestException as e:
         raise ValueError("Connection issue with myxine server (is it running?):", e)
@@ -109,12 +99,8 @@ def update(path : str,
     """Set the contents of the page at the given path to a provided body and
     title. If body or title is not provided, clears those elements of the page.
     """
-    try:
-        url = page_url(path, port)
-        params : Dict[str, str]
-        if title is None: params = {}
-        else: params = {'title': title}
-        r = requests.post(url, data=body.encode(), params=params)
+    url = page_url(path, port)
+    try: requests.post(url, data=body.encode(), params={'title': title})
     except RequestException as e:
         raise ValueError("Connection issue with myxine server (is it running?):", e)
 
@@ -126,8 +112,7 @@ def static(path : str,
     provided, as a bytestring. You must specify a content type, or else the
     browser won't necessarily know how to display this content.
     """
-    try:
-        url = page_url(path, port) + '?static'
-        requests.post(url, data=body, headers={'Content-Type': content_type})
+    url = page_url(path, port) + '?static'
+    try: requests.post(url, data=body, headers={'Content-Type': content_type})
     except RequestException as e:
         raise ValueError("Connection issue with myxine server (is it running?):", e)
