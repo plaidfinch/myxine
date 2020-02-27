29040c3e3218b71c5484fa823e47637f72e6c211
Kenny Foner <kwf@galois.com>
2020-02-27 17:31:12 -0500

    Update Python client to simpler JSON-lines interface

--- clients/python/myxine.py
+++ clients/python/myxine.py
@@ -1,49 +1,8 @@
 from typing import Optional, Iterator, Dict, List, Any
-from copy import deepcopy
-from dataclasses import dataclass
 import requests
+from dataclasses import dataclass
 from requests import RequestException
 import json
-
-
-@dataclass
-class Event:
-    """An event, as parsed from a text/event-stream"""
-    event_type: str = ''
-    event_id: str = ''
-    event_data:  str = ''
-
-
-def parse_event_stream(lines: Iterator[str]) -> Iterator[Event]:
-    """Parse an iterable of lines from the text/event-stream format, as
-    defined by the W3C Recommendation on Server-Sent Events:
-    https://www.w3.org/TR/eventsource/#event-stream-interpretation
-    """
-    data_lines: List[str] = []
-    event = Event()
-    for line in lines:
-        if line == '':
-            # Dispatch the event
-            event.event_data = '\n'.join(data_lines)
-            yield event
-            # Reset and continue
-            event, data_lines = Event(), []
-        else:
-            # Extract the field name and value
-            try:
-                field, value = line.split(':', maxsplit=1)
-            except ValueError:
-                field, value = line, ''
-            if value.startswith(' '):
-                value = value[1:]
-
-            # Set the appropriate field of the event, if any
-            if field == 'event':
-                event.event_type = value
-            elif field == 'id':
-                event.event_id = value
-            elif field == 'data':
-                data_lines.append(value)
 
 
 # The default port on which myxine operates; can be overridden in the below
@@ -51,45 +10,30 @@ def parse_event_stream(lines: Iterator[str]) -> Iterator[Event]:
 MYXINE_DEFAULT_PORT = 1123
 
 
-class PageEvent:
-    """A PageEvent is a special case of an event from a text/event-stream,
-    where the data is a dictionary from strings to values, representing the
-    fields requested by the subscription.
+@dataclass
+class Target:
+    """A Target corresponds to an element in the browser's document. It
+    contains a tag name and a mapping from attribute name to attribute value.
     """
-    type:   str
-    target: str
-    __data: str
-    __mapping: Optional[Dict[str, Any]] = None
-    __immutable: bool = False
+    tag: str
+    attributes: Dict[str, str]
 
-    def __init__(self, wrapped: Event) -> None:
-        self.type = wrapped.event_type
-        self.target = wrapped.event_id
-        self.__data = wrapped.event_data
-        self.__immutable = True
 
-    # Accessing fields looks them up in the dictionary
-    def __getattr__(self, key: str) -> Any:
-        if self.__mapping is None:
-            # Delay parsing until first lookup, then cache results
-            try:
-                self.__mapping = json.loads(self.__data)
-            except json.JSONDecodeError:
-                self.__mapping = {}
-        val = self.__mapping.get(key)
-        if val is not None:
-            return deepcopy(val)
+@dataclass
+class Event:
+    """An Event from a page has a type, a list of targets, and a set of
+    properties keyed by strings, which may be any type.
+    """
+    type: str
+    targets: List[Target]
+    properties: Dict[str, Any]
+
+    def __getattr__(self, name) -> Any:
+        value = self.properties[name]
+        if value is None:
+            raise AttributeError
         else:
-            msg = f"{self.__class__.__name__} object has no attribute {key}"
-            raise AttributeError(msg)
-
-    # Block setting attributes
-    def __setattr__(self, key: str, val: str) -> None:
-        if self.__immutable and key != '_PageEvent__mapping':
-            msg = f"{self.__class__.__name__} is immutable: {key} can't be set"
-            raise AttributeError(msg)
-        else:
-            super().__setattr__(key, val)
+            return value
 
 
 def page_url(path: str, port: int = MYXINE_DEFAULT_PORT) -> str:
@@ -101,7 +45,7 @@ def page_url(path: str, port: int = MYXINE_DEFAULT_PORT) -> str:
 
 def events(path: str,
            subscription: Optional[List[str]] = None,
-           port: int = MYXINE_DEFAULT_PORT) -> Iterator[PageEvent]:
+           port: int = MYXINE_DEFAULT_PORT) -> Iterator[Event]:
     """Subscribe to a stream of page events from a myxine server, returning an
     iterator over the events returned by the stream as they become available.
     """
@@ -116,10 +60,16 @@ def events(path: str,
         response = requests.get(url, stream=True, params=params)
         if response.encoding is None:
             response.encoding = 'utf-8'
-        stream = parse_event_stream(response.iter_lines(decode_unicode=True))
-        for event in stream:
-            if event.event_data != '':  # filter out heartbeat events
-                yield PageEvent(event)
+        for line in response.iter_lines(decode_unicode=True):
+            try:
+                parsed = json.loads(line)
+                yield Event(type=parsed['event'],
+                            targets=[Target(tag=j['tagName'],
+                                            attributes=j['attributes'])
+                                     for j in parsed['targets']],
+                            properties=parsed['properties'])
+            except json.JSONDecodeError:
+                pass
     except RequestException as e:
         msg = "Connection issue with myxine server (is it running?)"
         raise ValueError(msg, e)
