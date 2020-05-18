5feeb07bb2b1d41b7386877abc4b7e4c21d419cd
Kenny Foner <kwf@galois.com>
2020-05-18 11:50:49 -0400

    Correctly export relevant pieces of myxine Python module

---
+++ clients/python/myxine/__init__.py
@@ -0,0 +1,10 @@
+from . import myxine
+
+Target = myxine.Target
+Event = myxine.Event
+
+page_url = myxine.page_url
+events = myxine.events
+evaluate = myxine.evaluate
+update = myxine.update
+static = myxine.static
